c8bb6362558f877acb74afb275cec61fc7160dd3
Murilo Castilho <murilocastilho67@gmail.com>
2024-08-29 21:47:13 -0300

    Classe cadastrar doença criada

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Doenca(models.Model):
+    nomeDoenca = models.CharField(max_length=80)
+    sintomas = models.CharField(max_length=80)
+    trat_associados = models.CharField(max_length=80)
+
+
