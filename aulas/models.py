1e276745f655a2fa5d12b908afd881fb2c7231f7
Kennedy Gustavo De Oliveira <kennedy.oliveira@acessoline.intra>
2024-08-29 22:04:20 -0300

    entidades

--- aulas/models.py
+++ aulas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class FunCadastrarAnimal(models.Model):
+    nome = models.CharField(max_length=80)
+    idtutor = models.IntegerField(max_length=80)
+    raca = models.CharField(max_length=80)
+    idade = models.IntegerField(max_length=3)
+    especie = models.CharField(max_length=80)
+    doenca = models.BooleanField(max_length=10)
+    qual = models.CharField(max_length=80)
