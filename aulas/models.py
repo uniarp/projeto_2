3e6e4ce3c6b36edde45ab89bf35824b2a8012f17
beto <humbertopohlenz@gmail.com>
2024-10-10 21:28:12 -0300

    AdicionadoClasseTUTOR

--- aulas/models.py
+++ aulas/models.py
@@ -6,4 +6,18 @@ class Doenca(models.Model):
     sintomas = models.CharField(max_length=80)
     trat_associados = models.CharField(max_length=80)
 
+class Tutor(models.Model):
+    nome = models.Charfield(max_lenght=80)
+    sobrenome = models.Charfield(max_lenght=80)
+    rua = models.Charfield(max_lenght=80)
+    bairro = models.Charfield(max_lenght=80)
+    num = models.Charfield(max_lenght=8)
+    cidade = models.Charfield(max_lenght=80)
+    estado = models.Charfield(max_lenght=80)
+    cep = models.Charfield(max_lenght=12)
+    email = models.Charfield(max_lenght=60)
+    telefone = models.Charfield(max_lenght=12)
+    cpf = models.Charfield(max_lenght=14)
+    idTutor = models.IntegerField((max_lenght=80)primary_key=true)
+
 
